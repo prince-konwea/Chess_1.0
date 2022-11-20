5ac0e52322713b394f54a82ecd2f87d422307e17
prince-konwea <kokodecontee123@gmail.com>
2022-11-20 22:38:59 +0100

    created initial CRA Template and  installed react-dnd and rsjx dependecies, created a game and board file for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css"
+import {gameSubject} from "./Game"
 
 function App() {
+  const [board, setBoard] = useState([])
+  useEffect(() => {
+    const subscribe = gameSubject.subscribe(game => setBoard(game.board))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
---
+++ src/Board.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+function Board(){
+    return <div>
+
+    </div>
+}
+
+export default Board;
